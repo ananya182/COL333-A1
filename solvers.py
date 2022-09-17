7f628623389a1c28951c48894b0fdbe6adf7b68b
Ishaan685 <ishaangovil149@gmail.com>
2022-09-18 01:40:46 +0530

    Updated

--- solvers.py
+++ solvers.py
@@ -12,6 +12,7 @@ class SentenceCorrector(object):
         """
         # You should keep updating self.best_state with best string so far.
         self.best_state = start_state
+        print("Initial Sentence :",self.best_state)
         print("Initial Cost :", self.cost_fn(self.best_state))
 
         mylist = start_state.split(" ")
@@ -19,16 +20,38 @@ class SentenceCorrector(object):
             temp = mylist[i]
             for j in range(len(temp)):
                 ch = temp[j]
-                for k in self.conf_matrix[ch]:
-                    temp = temp[:j] + k + temp[j+1:]
-                    print(temp, self.cost_fn(temp))
-                    if(self.cost_fn(temp) < self.cost_fn(mylist[i])):
-                        mylist[i] = temp
-                        self.best_state = " ".join(mylist)
-                    else:
-                        temp = temp[:j] + ch + temp[j+1:]
+                temp = temp[:j] + ch + temp[j+1:]
+                # print(temp, self.cost_fn(temp))
+                for k in self.conf_matrix.keys():
+                    if(ch in self.conf_matrix[k]):
+                # for k in self.conf_matrix[ch]:
+                        temp = temp[:j] + k + temp[j+1:]
+                        
+                        if(i != 0 and i != len(mylist) - 1):
+                            if(self.cost_fn(mylist[i-1] +" "+ temp + " "+ mylist[i+1]) < self.cost_fn(mylist[i-1] +" "+ mylist[i] +" "+ mylist[i+1])):
+                                mylist[i] = temp
+                                self.best_state = " ".join(mylist)
+                            else:
+                                temp = temp[:j] + ch + temp[j+1:]
+                            # print(mylist[i-1] + temp + mylist[i+1], self.cost_fn(temp))
+                        elif(i == 0):
+                            if(self.cost_fn(temp +" "+ mylist[i+1] +" "+ mylist[i+2]) < self.cost_fn(mylist[i] +" "+ mylist[i+1] +" "+ mylist[i+2])):
+                                mylist[i] = temp
+                                self.best_state = " ".join(mylist)
+                            else:
+                                temp = temp[:j] + ch + temp[j+1:]
+                        else :
+                            if(self.cost_fn(mylist[i-2] +" "+ mylist[i-1] +" "+ temp ) < self.cost_fn(mylist[i-2] +" "+ mylist[i-1] +" "+ mylist[i])):
+                                mylist[i] = temp
+                                self.best_state = " ".join(mylist)
+                            else:
+                                temp = temp[:j] + ch + temp[j+1:]
+                        # print(temp)
+
+
 
 
         # self.best_state = start_state
+        print("Final Sentence :",self.best_state)
         print("Final Cost :", self.cost_fn(self.best_state))
         # raise Exception("Not Implemented.")
